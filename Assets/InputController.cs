d4d632d655854942f3218bba86fa86dd0fc3624e
Tom Florkiewicz <tflorkiewicz@gmail.com>
2014-04-07 21:32:46 -0400

    Initial commit

---
+++ Assets/InputController.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+    private bool _isDrawing = false;
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    if (Application.isEditor)
+	    {
+	        if (Input.GetMouseButtonDown(0) && !_isDrawing)
+	        {
+	            Debug.Log(Input.mousePosition);
+	            RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+                Debug.Log(hit);
+	            if (hit.collider != null)
+	            {
+	                Debug.Log("Hit object: " + hit.transform.name);
+	                var pathController = hit.transform.parent.GetComponent<PathController>();
+	                if (pathController == null)
+	                {
+	                    Debug.Log("Cannot locate PathController");
+	                    return;
+	                }
+
+                    if (string.Equals(hit.transform.name, "Waypoint_" + pathController.CurrentWaypoint))
+                    {
+                        _isDrawing = true;
+                        pathController.Advance();
+                    }
+	            }
+	        }
+            if (Input.GetMouseButton(0) && _isDrawing)
+            {
+                Debug.Log("pressing mouse button");
+            }
+	        return;
+	    }
+
+	    var touches = Input.touches;
+	    foreach (var touch in touches)
+	    {
+           Debug.Log(touch.position);
+	       if (touch.phase == TouchPhase.Began)
+	       {
+               Debug.Log("Began touch");
+	           RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(touch.position), Vector2.zero);
+
+               if (hit.collider != null)
+               {
+                   Debug.Log("Hit object: " + hit.transform.name);
+               }
+	       }
+	    }
+	}
+}
---
+++ Assets/PathController.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PathController : MonoBehaviour
+{
+    public int CurrentWaypoint = 1;
+
+    // Use this for initialization
+	void Start () 
+    {
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+    }
+
+    public void Advance()
+    {
+        var waypoint1 = transform.FindChild("Waypoint_" + CurrentWaypoint);
+        var waypoint2 = transform.FindChild("Waypoint_" + (CurrentWaypoint + 1));
+
+        var pathCollider = transform.FindChild("PathCollider");
+
+        var deltaX = waypoint2.transform.position.x - waypoint1.transform.position.x;
+        var deltaY = waypoint2.transform.position.y - waypoint1.transform.position.y;
+
+        var hyp = Mathf.Sqrt(deltaX*deltaX + deltaY*deltaY);
+        var midX = waypoint1.transform.position.x + 0.5f*deltaX;
+        var midY = waypoint1.transform.position.y + 0.5f*deltaY;
+
+        pathCollider.transform.position = new Vector3(midX, midY);
+        pathCollider.transform.localScale = new Vector3(1, hyp);
+
+        //Vector3 temp = pathCollider.transform.localEulerAngles;
+        pathCollider.transform.LookAt(waypoint2.transform.position);
+        //temp.y = pathCollider.transform.localEulerAngles.y;
+        //pathCollider.transform.localEulerAngles = temp;
+
+        CurrentWaypoint += 1;
+    }
+}
+
