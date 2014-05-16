0d70349f9415736861ff6ad8c44dfb571546fcfc
TOM FLORKIEWICZ <tflorkiewicz@gmail.com>
2014-05-15 22:06:25 -0400

    added particle effects on waypoints

---
+++ Assets/Scripts/InputController.cs
@@ -0,0 +1,86 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputController : MonoBehaviour 
+{
+    public GameObject Pen;
+    public float DrawPause;
+
+    private bool _isDrawing = false;
+    private float _lastDrawTime;
+    private PathController _pathController;
+
+    void Start()
+    {
+        _lastDrawTime = Time.time;
+    }
+
+    private bool IsUserFirstClickOrTouch()
+    {
+        //todo: implement for touches
+        return Application.isEditor ? Input.GetMouseButton(0) : false;
+    }
+    private bool IsUserContinuousClickOrTouch()
+    {
+        //todo: implement for touches
+        return Application.isEditor ? Input.GetMouseButton(0) : false;
+    }
+
+    private Vector3 InteractionWorldPoint()
+    {
+        //todo: implement for touches
+        return Application.isEditor ? Camera.main.ScreenToWorldPoint(Input.mousePosition) : Vector3.zero;
+    }
+	
+	void Update ()
+	{
+        if (IsUserFirstClickOrTouch() && !_isDrawing)
+	    {
+            RaycastHit2D hit = Physics2D.Raycast(InteractionWorldPoint(), Vector2.zero);
+	        if (hit.collider != null)
+	        {
+	            _pathController = hit.transform.parent.GetComponent<PathController>();
+                if (_pathController == null)
+	            {
+	                Debug.Log("Fatal error: Cannot locate PathController");
+	                return;
+	            }
+                // If user clicked the 'next' waypoint
+                if (string.Equals(hit.transform.name, "Waypoint_" + _pathController.CurrentWaypoint))
+                {
+                    _isDrawing = true;
+                    _pathController.Advance();
+                }
+	        }
+            return;
+	    }
+
+        if (IsUserContinuousClickOrTouch() && _isDrawing)
+        {
+            RaycastHit2D hit = Physics2D.Raycast(InteractionWorldPoint(), Vector2.zero);
+                
+            // drawing using the pen field if inside the pathcollider object
+            if (hit.collider != null && hit.transform.name == "PathCollider")
+            {
+                if (Time.time > _lastDrawTime + DrawPause)
+                {
+                    var mark = Instantiate(Pen, hit.point, Quaternion.identity) as GameObject;
+                    _pathController.AddPenMark(mark.transform);
+                    _lastDrawTime = Time.time;
+                }
+            }
+            // hit the next waypoint!
+            else if (hit.collider != null && hit.transform.name.StartsWith("Waypoint_" + _pathController.CurrentWaypoint))
+            {
+                _pathController.Advance();
+            }
+        }
+        
+        if (!IsUserContinuousClickOrTouch() && _isDrawing)
+        {
+            Debug.Log("Stopped drawing :(");
+            _isDrawing = false;
+            _pathController.Retreat();
+        }
+	}
+}
---
+++ Assets/Scripts/Pulse.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pulse : MonoBehaviour {
+
+    public float RangePercentage = 10f;
+    public float CycleTime = 1.0f;
+
+    private int _sign = 1;
+    private float _timer;
+    private float _lastSwitch;
+    private Vector3 _originalSize;
+
+	// Use this for initialization
+	void Start () {
+        _originalSize = this.transform.localScale;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        _timer = Time.time;
+        if (_lastSwitch == null || _timer > _lastSwitch + (CycleTime / 2f))
+        {
+            LeanTween.scaleX(this.gameObject, _sign * 1 * _originalSize.x + (RangePercentage / 100f), CycleTime / 2.0f);
+            LeanTween.scaleY(this.gameObject, _sign * 1 * _originalSize.y + (RangePercentage / 100f), CycleTime / 2.0f);
+            _lastSwitch = Time.time;
+            _sign *= -1;
+        }
+        
+	}
+}
